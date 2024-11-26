b1ce36e7505e36de7e475659824d6012ff1b4a79
JhonyP14 <jhony.paredes7583@utc.edu.ec>
2024-11-26 08:37:09 -0500

    add: Styles

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// script.js
 
-// Ejemplo de función que podría usarse para actualizar la descripción
 function actualizarPerfil(nombre, descripcion) {
     document.getElementById('nombre').textContent = nombre;
     document.getElementById('descripcion').textContent = descripcion;
 }
 
-// Llamar a la función para actualizar el perfil (puedes modificar los valores)
+
 actualizarPerfil("Paredes Jhony", "Ingeniero en Sistemas de la Información.");
